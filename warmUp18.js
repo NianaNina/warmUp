97eea10cec7142c2ac7b56a428a93043d96cbf4c
ons-rahmani <rahmanions@gmail.com>
2019-11-09 09:43:27 +0100

    wrm18

--- warmUp18.js
+++ warmUp18.js
@@ -1,6 +1,9 @@
+
+
 // You are given an input string.
 
-// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+// For each symbol in the string if it's the first character occurence, replace it with a '1',
+// else replace it with the amount of times you've already seen it...
 
 // But will your code be performant enough?
 // Examples:
@@ -10,3 +13,18 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+  function countOccurrences(string, character) {
+
+  	    var str = string.split(' ');
+       if (Array.isArray()) {
+      for (var i = 0; i < string.length; i++) {
+        character(string[i], i);
+      }
+    } else {
+      for (var key in string) {
+        character(string[key], key);
+      }
+    }
+  }
+  
